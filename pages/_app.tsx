179f5d56e78986a21ad9ecaba173e62fd9bf472c
carloshs1 <carlos@ebless.mx>
2022-10-15 18:48:47 -0500

    Add supabase db, stepzen to hadle graphql and apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,23 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import { Session } from 'next-auth'
+import Header from '../components/Header'
+import { ApolloProvider } from '@apollo/client'
+import client from '../apollo-client'
 
 function MyApp({
  Component,
  pageProps: { session, ...pageProps },
 }: AppProps<{ session: Session }>) {
  return (
-  <SessionProvider session={session}>
-   <Component {...pageProps} />
-  </SessionProvider>
+  <ApolloProvider client={client}>
+   <SessionProvider session={session}>
+    <div className="h-screen overflow-y-scroll bg-slate-200">
+     <Header />
+     <Component {...pageProps} />
+    </div>
+   </SessionProvider>
+  </ApolloProvider>
  )
 }
 
